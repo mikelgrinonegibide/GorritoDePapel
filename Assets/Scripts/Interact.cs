86d0211367c7ad5277aee59795f57b7109e0abdc
Alexator <alexatorlulu@gmail.com>
2024-12-11 17:17:04 +0100

    empezando interactuar

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interact : MonoBehaviour
+{
+
+    public Text dialogo;
+  
+
+   public void gun()
+   {
+    dialogo.text="pistola";
+   }
+public void photo()
+{
+    dialogo.text="foto de pareja";
+}
+
+
+
+    }
+
